bb8c72f2c0210a82b19fab6dc14a51d7f256301e
Yannick Schutz <yannick.schutz@gmail.com>
2014-01-01 01:18:56 +0100

    Add help

--- http.go
+++ http.go
@@ -32,8 +32,13 @@ func getCodeshipStatus(w http.ResponseWriter, r *http.Request) {
   fmt.Fprint(w, fmt.Sprintf("{\"status\": \"%s\"}", status))
 }
 
+func index(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprint(w, "go to /{codeship_projectUUID}/{branch} to get a json status of your project. You can find the UUID in configuration/General of codeship project")
+}
+
 func router() *mux.Router {
   r := mux.NewRouter()
+  r.HandleFunc("/", index).Methods("GET")
   r.HandleFunc("/{projectUUID}/{branch:.+}", getCodeshipStatus).Methods("GET")
   return r
 }
