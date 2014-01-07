931a1a3a47797a8150680610ae5bc527e89c1307
Yannick Schutz <yannick.schutz@gmail.com>
2014-01-07 09:05:52 +0100

    Use head

--- http.go
+++ http.go
@@ -15,7 +15,7 @@ func routerHandlerFunc(router *mux.Router) http.HandlerFunc {
 
 func getStatus(projectUUID string, branch string) string {
   codeshipURL := fmt.Sprintf("https://www.codeship.io/projects/%s/status?branch=%s", projectUUID, branch)
-  resp, _ := http.Get(codeshipURL)
+  resp, _ := http.Head(codeshipURL)
   contentDisposition := resp.Header.Get("Content-Disposition")
   re, _ := regexp.Compile("inline; filename=\"status_(?P<status>.+).png\"")
   status := re.FindStringSubmatch(contentDisposition)[1]
